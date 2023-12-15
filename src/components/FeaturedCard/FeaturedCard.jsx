7eb632edbbed0e6485fa81d593d968e7489f02cd
Ramirof <ramirofiscella@gmail.com>
2023-12-15 12:26:13 -0300

    featured and footer base

--- src/components/FeaturedCard/FeaturedCard.jsx
+++ src/components/FeaturedCard/FeaturedCard.jsx
@@ -4,7 +4,18 @@ import style from "./FeaturedCard.module.css";
 const FeaturedCard = () => {
   return (
     <div className={style.featuredCard}>
-      <h1>Featured Card</h1>
+      <div className={style.featuredCardText}>
+        <h2>Featured Card</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod
+          perferendis incidunt ducimus molestias dolor cum aliquam facere. Est,
+          saepe?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+          voluptatum?
+        </p>
+      </div>
       <div className={style.featuredCardImg}></div>
     </div>
   );
