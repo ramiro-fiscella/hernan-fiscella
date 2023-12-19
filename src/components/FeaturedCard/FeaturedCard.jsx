d8b67a7804613c2a74b5832aacdd5208909bd2ec
Ramirof <ramirofiscella@gmail.com>
2023-12-19 14:36:56 -0300

    footer

--- src/components/FeaturedCard/FeaturedCard.jsx
+++ src/components/FeaturedCard/FeaturedCard.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import style from "./FeaturedCard.module.css";
 
 const FeaturedCard = () => {
+  const cardRef = useRef(null);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseMove = (e) => {
+    const card = cardRef.current;
+    const img = card.querySelector(`.${style.featuredCardImg}`);
+
+    const xAxis = (window.innerWidth / 2 - e.pageX) / 50; // Controla el movimiento horizontal
+    const yAxis = (window.innerHeight / 2 - e.pageY) / 50; // Controla el movimiento vertical
+    img.style.transform = `translate(${xAxis}px, ${yAxis}px)`;
+  };
+
+  const handleMouseLeave = () => {
+    const card = cardRef.current;
+    const img = card.querySelector(`.${style.featuredCardImg}`);
+
+    img.style.transition = "transform 0.5s ease-in-out";
+    img.style.transform = "translate(0, 0)";
+    setIsHovered(false);
+  };
+
+  const handleMouseEnter = () => {
+    const card = cardRef.current;
+    const img = card.querySelector(`.${style.featuredCardImg}`);
+
+    setIsHovered(true);
+  };
+
   return (
-    <div className={style.featuredCard}>
+    <div
+      className={style.featuredCard}
+      ref={cardRef}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      onMouseEnter={handleMouseEnter}
+    >
+      <div className={style.featuredCardImg}></div>
       <div className={style.featuredCardText}>
-        <h2>Featured Card</h2>
+        <h2>Featured Work</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod
           perferendis incidunt ducimus molestias dolor cum aliquam facere. Est,
@@ -15,8 +50,8 @@ const FeaturedCard = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
           voluptatum?
         </p>
+        <button className={style.workBtn}>View Work</button>
       </div>
-      <div className={style.featuredCardImg}></div>
     </div>
   );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,64 @@ import style from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <div className={style.footerContainer}>
-      <div className={style.footer}></div>
-    </div>
+    <footer className={style.footer}>
+      <div className={style.footerContainer}>
+        <div className={style.footerTop}>
+          <div>
+            <h1>HERNAN FISCELLA</h1>
+            <p>Photographer & Videographer</p>
+          </div>
+          <div className={style.footerImg}></div>
+        </div>
+
+        <div className={style.footerMiddle}>
+          <div className={style.middleContainer}>
+            <div className={style.middleLinks}>
+              <h3>Adress</h3>
+              <a href="#">
+                Street 564 - 23 <br /> CP7300 <br /> Azul, Buenos Aires,
+                Argentina
+              </a>
+            </div>
+          </div>
+
+          <div className={style.middleContainer}>
+            <div className={style.middleLinks}>
+              <h3>Links</h3>
+              <a href="#">Home</a>
+              <a href="#">Work</a>
+              <a href="#">About</a>
+            </div>
+          </div>
+
+          <div className={style.middleContainer}>
+            <div className={style.middleLinks}>
+              <h3>Socials</h3>
+              <a href="#">Instagram</a>
+              <a href="#">Facebook</a>
+            </div>
+          </div>
+
+          <div className={style.middleContainer}>
+            <div className={style.middleLinks}>
+              <h3>Contact</h3>
+              <a href="#">hernanfiscella@photographer.com</a>
+            </div>
+          </div>
+        </div>
+
+        <div className={style.footerBottom}>
+          <p>© 2023 HERNAN FISCELLA</p>
+
+          <div className={style.developer}>
+            <p>Designed & Developed by</p>
+            <a target="_blank" href="https://ramirof.vercel.app/">
+              Ramiro Fiscella
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
