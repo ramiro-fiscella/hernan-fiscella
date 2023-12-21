06e332986ae1683147f46a839b122e65afca7d4b
Ramirof <ramirofiscella@gmail.com>
2023-12-21 16:10:11 -0300

    work WorkSections

---
+++ src/components/WorkSections/WorkSections.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import style from "./WorkSections.module.css";
+
+const WorkSections = () => {
+  return (
+    <div className={style.workContainer}>
+      <div className={style.workText}>
+        <h1>WORK STYLES</h1>
+        <p>
+          From capturing the raw emotion of photojournalism to the intricate
+          composition of still life, their lens unveils diverse worlds. <br />
+          Portraits freeze moments of human connection, while landscapes unveil
+          the grandeur of nature. <br />
+          Fashion photography merges creativity and design, while abstract
+          imagery sparks imagination. <br />
+          <br />
+          Through their lens, photographers explore, evoke, and immortalize
+          moments in time, weaving a tapestry of visual narratives.
+        </p>
+        {/* <p>Doing fun stuff is great. But doing something great is more fun.</p> */}
+      </div>
+
+      <div className={style.stylesContainer}>
+        <div className={style.wedding}>
+          <div className={style.weddingImg}>
+            <div className={style.weddingText}>
+              <h2>WEDDING</h2>
+              <p>Wedding - Birthday - Celebration </p>
+            </div>
+          </div>
+        </div>
+
+        <div className={style.business}>
+          <div className={style.businessImg}>
+            <div className={style.businessText}>
+              <h2>BUSINESS</h2>
+              <p>Fashion - Product - Stock</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkSections;
